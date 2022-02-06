9cd51e01f4bbed9e93db49e7a4104675d11d0dd4
igorkll <69525332+igorkll@users.noreply.github.com>
2022-02-06 12:27:57 +0300

    Add files via upload

--- nano.lua
+++ nano.lua
@@ -1,123 +1,45 @@
-local component = require("component")
-local event = require("event")
+local nano = require("nano")
+local term = require("term")
 
------------------------------------------
+---------------------------------------------------
 
-local function getModem()
-    for address in component.list("modem") do
-        local proxy = component.proxy(address)
-        if proxy.isWireless() == true then
-            return proxy
+local function input(message)
+    term.write((message or "select")..": ")
+    return io.read() or os.exit()
+end
+
+local function toboolean(data)
+    if data == 0 or data == nil or data == false or data == "false" or data == "0" then return false end
+    if data == "true" or data == true or data == 1 or data == "1" then return true end
+end
+
+local function wait()
+    print("press enter to continue...")
+    io.read()
+end
+
+---------------------------------------------------
+
+
+while true do
+    term.clear()
+    print("1.set pin")
+    print("2.get pin")
+    print("3.get pins")
+    local select = input()
+    if select == "1" then
+        local pin = tonumber(input("pin"))
+        local state = toboolean(input("state"))
+        nano.setInput(pin, state)
+    elseif select == "2" then
+        local pin = tonumber(input("pin"))
+        print(nano.getInput(pin))
+        wait()
+    elseif select == "3" then
+        local pinsCount = nano.getTotalInputCount()
+        for i = 1, pinsCount do
+            print(tostring(i).."."..tostring(nano.getInput(i)))
         end
+        wait()
     end
-    error("wireless modem is not available")
 end
-
------------------------------------------
-
-local lib = {}
-
-local ok = false
-lib.isOk = function()
-    return ok
-end
-
-lib.raw_send = function(...)
-    local modem = getModem()
-    local port = math.random(1, 65535)
-    local strength = modem.setStrength(2)
-    local isOpen = modem.open(port)
-
-    modem.broadcast(port, "nanomachines", "setResponsePort", port)
-    local eventName = event.pull(4, "modem_message", modem.address, nil, port, nil, "nanomachines", "port", port)
-    if not eventName then
-        ok = false
-        modem.setStrength(strength)
-        if isOpen == true then
-            modem.close(port)
-        end
-        return
-    end
-    modem.broadcast(port, "nanomachines", ...)
-
-    modem.setStrength(strength)
-    local data = table.pack(event.pull(4, "modem_message", modem.address, nil, port, nil, "nanomachines"))
-    if isOpen == true then
-        modem.close(port)
-    end
-
-    local returnData = {}
-    for i = 7, #data do
-        returnData[#returnData + 1] = data[i]
-    end
-    ok = true
-    return table.unpack(returnData)
-end
-
---------------------
-
-lib.getInput = function(num)
-    local _, _, out = lib.raw_send("getInput", num)
-    return out
-end
-
-lib.setInput = function(num, state)
-    lib.raw_send("setInput", num, state)
-end
-
-lib.getActiveEffects = function()
-    local _, out = lib.raw_send("getActiveEffects")
-    return out
-end
-
-lib.getMaxActiveInputs = function()
-    local _, out = lib.raw_send("getMaxActiveInputs")
-    return out
-end
-
-lib.getSafeActiveInputs = function()
-    local _, out = lib.raw_send("getSafeActiveInputs")
-    return out
-end
-
-lib.getTotalInputCount = function()
-    local _, out = lib.raw_send("getTotalInputCount")
-    return out
-end
-
-lib.getExperience = function()
-    local _, out = lib.raw_send("getExperience")
-    return out
-end
-
-lib.getName = function()
-    local _, out = lib.raw_send("getName")
-    return out
-end
-
-lib.getHunger = function()
-    local _, out1, out2 = lib.raw_send("getHunger")
-    return out1, out2
-end
-
-lib.getAge = function()
-    local _, out = lib.raw_send("getAge")
-    return out
-end
-
-lib.getHealth = function()
-    local _, out = lib.raw_send("getHealth")
-    return out
-end
-
-lib.getPowerState = function()
-    local _, out1, out2 = lib.raw_send("getPowerState")
-    return out1, out2
-end
-
-lib.saveConfiguration = function()
-    local _, out1, out2 = lib.raw_send("saveConfiguration")
-    return out1, out2
-end
-
-return lib
